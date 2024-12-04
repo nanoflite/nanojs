628f583741fb070f39a2fc52625888ff3cad2b7c
Johan Van den Brande <johan@vandenbrande.com>
2024-12-04 21:51:37 +0100

    Add a component feature

---
+++ c.mjs
@@ -0,0 +1,27 @@
+// component
+
+import { add } from './n.mjs'
+
+class Component extends HTMLElement {
+    constructor(app, props) {
+        super();
+        this.app = app ? app : _ => 'nothing here'
+        this.props = props ? props : {}
+        this.shadow = this.attachShadow({ mode: 'open' })
+    }
+
+    connectedCallback() {
+        add(this.shadow, this.app(this.props))
+    }
+}
+
+function component(name, app) {
+    if (!window.customElements.get(name)) {
+        const Komponent = class extends Component {}
+        window.customElements.define(name, Komponent)
+    }
+    const klass = window.customElements.get(name)
+    return (props) => new klass(app, props)
+}
+
+export { component }
