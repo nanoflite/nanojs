5b0ccd96f9db8ae17fcad70d9a6aeafbba522ff1
Johan Van den Brande <johan@vandenbrande.com>
2025-01-28 22:37:50 +0100

    Renamed $ to S, added attr, html, text and fixed a bug so we can use e.g. appendChild.

--- nanojs/index.mjs
+++ nanojs/index.mjs
@@ -1,11 +1,11 @@
 import { tags, add } from './dom.mjs'
 import { state, states, watch, derive, change, until } from './state.mjs'
-import { sleep, schedule, css, $ } from './utils.mjs'
+import { sleep, schedule, css, S } from './utils.mjs'
 import { router } from './router.mjs'
 import { model } from './model.mjs'
 import { component } from './component.mjs'
 
-export { tags, add, state, states, watch, derive, change, until, sleep, schedule, css, $, router, model, component }
+export { tags, add, state, states, watch, derive, change, until, sleep, schedule, css, S, router, model, component }
 
 console.log(`
             nanojs
--- nanojs/utils.mjs
+++ nanojs/utils.mjs
@@ -8,24 +8,24 @@ const css = (strings, ...values) => {
     return strings.raw.reduce((acc, str, i) => acc + str + (values[i] || ""), "");
 }
 
-const $ = (selector, context = document) => {
+const S = (selector, context = document) => {
     const elements = (context.shadowRoot ?? context).querySelectorAll(selector)
     return new Proxy(elements, {
         get(target, prop, receiver) {
-            if (prop === 'css') {
-                return (property, value) => {
-                    target.forEach(el => (el.style[property] = value))
-                    return target
-                }
+            const createFn = fn => (...args) => {
+                target.forEach(el => fn(el, ...args))
+                return target
             }
-            if (prop === 'on') {
-                return (event, handler) => {
-                    target.forEach(el => el.addEventListener(event, handler))
-                    return target
-                }
+            const fnMap = {
+                'css': (el, property, value) => (el.style[property] = value),
+                'on': (el, event, handler) => el.addEventListener(event, handler),
+                'attr': (el, name, value) => (el.setAttribute(name, value)),
+                'html': (el, html) => (el.innerHTML = html),
+                'text': (el, text) => (el.textContent = text)
             }
-            if (target.length === 1) return target[0][prop]
-            return Array.from(target).map(el => el[prop])
+            if (fnMap[prop]) return createFn(fnMap[prop])
+            return (...args) => Array.from(target,
+                (el) => (typeof el[prop] === 'function') ? el[prop].apply(el, args) : el[prop])
         },
         set(target, prop, value) {
             target.forEach(el => (el[prop] = value))
@@ -34,4 +34,4 @@ const $ = (selector, context = document) => {
     })
 }
 
-export { sleep, schedule, css, $ }
+export { sleep, schedule, css, S }
