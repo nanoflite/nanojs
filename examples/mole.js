2061a6dd6a5f3f7c9968ce40916309604dca399b
Johan Van den Brande <johan@vandenbrande.com>
2025-01-28 22:39:01 +0100

    Persist high score, tune gameplay and use 'S' selector to add to div with app id.

--- examples/mole.js
+++ examples/mole.js
@@ -1,18 +1,19 @@
-import { add, tags, states, sleep, watch, change } from "../nanojs/index.mjs"
+import { add, tags, state, states, sleep, watch, change, S } from "../nanojs/index.mjs"
 
 const { span, div, h1, button } = tags()
 
 const gamestate = states()
 const { score, timer, showscore, moles, running, lastwacked } = gamestate(0, 15, false, new Array(9).fill(false), false, -1)
 
-const mole = (i) => div(
-    {   class: 'mole',
-        onclick: _ => {
+const high = state(localStorage.getItem('high') || 0)
+watch( _ => { if (score.value > high.value) high.value = score.value, localStorage.setItem('high', high.value) })
+
+const mole = (i) => div({ class: 'mole', onclick: _ =>
+        {
             if (running.value && lastwacked.value !== i && moles.value[i]) score.value++
             lastwacked.value = i
         }
-    },
-    _ => moles.value[i] ? `🐹` : ` `)
+    }, _ => moles.value[i] ? `🐹` : ` `)
 
 const nextRandomMole = _ => {
     const i = Math.floor(Math.random() * moles.value.length)
@@ -26,9 +27,9 @@ const play = async () => {
     while (--timer.value > 0) {
         const theMole = nextRandomMole()
         clearMoles()
-        await sleep(200+Math.floor(Math.random()*500))
+        await sleep(200+Math.floor(Math.random()*600))
         moles.value = moles.value.map((_, i) => i === theMole)
-        await sleep(800)
+        await sleep(400+Math.floor(Math.random()*400))
     }
     clearMoles()
     showscore.value = true
@@ -36,18 +37,18 @@ const play = async () => {
     gamestate.reset()
 }
 
-add(document.body, () => div({ class: 'container' },
+add(S('#app'), () => div({ class: 'container' },
     div({ class: 'game' },
         h1('Whack-A-Mole'),
         div({ class: 'game-info'},
             span(_ => `Time: ${timer.value}s`),
-            span(_ => `Score: ${score.value}`)
+            span(_ => `Score: ${score.value}`),
+            span(_ => `High: ${high.value}`)
         ),
         div({class: 'grid'}, moles.value.map((_, i) => mole(i))),
-        button({
-            style: _ => running.value || showscore.value ? `background: #aaa` : `background: #007bff;`,
-            onclick: _ => running.value || play()
-        }, 'Start'),
+        button({ style: _ => running.value || showscore.value ? `background: #aaa` : `background: #007bff;`,
+                 onclick: _ => running.value || play()
+               }, 'Start'),
         div({ class: 'scoreboard', style: () => `display: ${showscore.value ? "block" : "none"};`},
             h1('Game Over!'),
             div(_ => `Score: ${score.value}`),
